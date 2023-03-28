6278182d9b090273a02a89e24201a9a733b83e91
Jensen Judkins <thesmelliestman@gmail.com>
2023-03-28 13:24:37 -0600

    added basic outline of everything we need

--- src/index.ts
+++ src/index.ts
@@ -24,24 +24,141 @@ feedingsController(app, client);
 husbandryController(app, client);
 schedulesController(app, client);
 
-//this is middleware for index page, shows login fields and register button
+
+
+//this is middleware for index page, shows 
+    // I should see the name of your application
+    // I should see a description of what the app does.
+    // I should be able to navigate to the Login page
+    // I should be able to navigate to the Signup page
+
+
 app.get("/", (req, res) => {
   res.send(`
     <h1>Reptile Manager</h1>
-    <form method="POST" action="/sessions">
+    <p>Reptile Manager is a web application that allows users to track their reptiles and their care.</p>
+    <a href="/login">Login</a>
+    <a href="/signup">Signup</a>
+  `);
+});
+
+//login page
+    // I should be able to sign into a user account
+    // I should be able to navigate to the signup page
+    // Upon signing in, I should be redirected to the dashboard page
+
+app.get("/login", (req, res) => {
+  res.send(`
+    <h1>Login</h1>
+    <form action="/sessions" method="POST">
       <input type="email" name="email" placeholder="Email" />
       <input type="password" name="password" placeholder="Password" />
       <button type="submit">Login</button>
     </form>
-    <form method="POST" action="/users/create">
+    <a href="/signup">Signup</a>
+    <a href="/">Home</a>
+  `);
+});
+
+//signup page  
+    // I should be able to create a user account
+    // I should be able to navigate to the Login page
+    // Upon creating an account I should be redirected to the dashboard page
+
+app.get("/signup", (req, res) => {
+  res.send(`
+    <h1>Signup</h1>
+    <form action="/users/create" method="POST">
       <input type="text" name="firstName" placeholder="First Name" />
       <input type="text" name="lastName" placeholder="Last Name" />
       <input type="email" name="email" placeholder="Email" />
       <input type="password" name="password" placeholder="Password" />
-      <button type="submit">Register</button>
+      <button type="submit">Signup</button>
     </form>
+    <a href="/login">Login</a>
+    <a href="/">Home</a>
   `);
 });
+
+//Dashboard Page
+    // I should see all of the schedules for my user for the day of the week it is (for example, if it is Monday then I should only see the schedules that have me doing something on Monday.)
+    // I should see a list of all my reptiles
+    // When selecting a reptile the app should navigate to the Reptile page
+    // I should be able to create a new reptile (you can do this on this page via something like a pop up, or you can create a new page for this)
+    // I should be able to delete a reptile.
+    // I should be able to log out of my account
+
+app.get("/dashboard", async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect("/login");
+    return;
+  }
+
+  const body = jwt.verify(token, process.env.ENCRYPTION_KEY!!) as JWTBody;
+  const user = await client.user.findUnique({
+    where: {
+      id: body.userId,
+    },
+  });
+
+  if (!user) {
+    res.redirect("/login");
+    return;
+  }
+
+  res.send(`
+    <h1>Dashboard</h1>
+    <p>Welcome ${user.firstName} ${user.lastName}</p>
+    <a href="/logout">Logout</a>
+    <a href="/">Home</a>
+  `);
+});
+
+//Reptile Page
+    // I should see a list of all of the feedings for this reptile
+    // I should see a list of all of the husbandry records for this reptile
+    // I should see a list of all of the schedules for this reptile.
+    // I should be able to update this reptile
+    // I should be able to create a feeding for this reptile
+    // I should be able to create a husbandry record for this reptile
+    // I should be able to create a schedule for this reptile
+
+app.get("/reptile/:id", async (req, res) => {
+  const reptile = await client.reptile.findUnique({
+    where: {
+      id: Number(req.params.id),
+    },
+  });
+
+  if (!reptile) {
+    res.status(404).send("Reptile not found");
+    return;
+  }
+
+  res.send(`
+    <h1>${reptile.name}</h1>
+    <p>Species: ${reptile.species}</p>
+    <p>Sex: ${reptile.sex}</p>
+
+    <a href="/logout">Logout</a>
+    <a href="/">Home</a>
+  `);
+});
+
+
+
+//logout page
+    // I should be able to log out of my account
+
+app.get("/logout", (req, res) => {
+  res.clearCookie("token");
+  res.redirect("/login");
+});
+
+
+
+
 
 //middleware for users, returns all users
 app.get("/users", async (req, res) => {
