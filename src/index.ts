4d7873412bf014ac071e1ff8540b334956c18428
Jensen Judkins <thesmelliestman@gmail.com>
2023-03-28 21:57:45 -0600

    added dashboard

--- src/index.ts
+++ src/index.ts
@@ -240,7 +240,50 @@ app.post("/users/login", async (req, res) => {
     user,
     token,
   });
+
+  
+
+
 });
+
+//middleware for dashboard
+  // Dashboard Page
+
+    // I should see all of the schedules for my user for the day of the week it is (for example, if it is Monday then I should only see the schedules that have me doing something on Monday.)
+    // I should see a list of all my reptiles
+    // When selecting a reptile the app should navigate to the Reptile page
+    // I should be able to create a new reptile (you can do this on this page via something like a pop up, or you can create a new page for this)
+    // I should be able to delete a reptile.
+    // I should be able to log out of my account
+
+app.get("/dashboard", async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    res.redirect("/login");
+    return;
+  }
+
+  const body = jwt.verify(token, process.env.ENCRYPTION_KEY!!) as JWTBody;
+  const user = await client.user.findUnique({
+    where: {
+      id: body.userId,
+    },
+  });
+
+  if (!user) { 
+    res.redirect("/login");
+    return;
+  }
+
+  res.send(`
+
+    <h1>Dashboard</h1>
+    <p>Welcome ${user.firstName} ${user.lastName}</p>
+    <a href="/logout">Logout</a>
+    <a href="/">Home</a>
+  `);
+});
+
 
 
 //middleware for user to register
